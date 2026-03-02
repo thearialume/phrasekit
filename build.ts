caf6d852cd3f041e41d7193238ac4a0cfc515bd5
Aria Lume <thearialume@gmail.com>
2026-03-02 19:34:35 +0500

    Update build target to node

--- build.ts
+++ build.ts
@@ -1,6 +1,7 @@
 await Bun.build({
-    entrypoints: ["./src/index.ts"],
-    outdir: "./dist",
-    minify: true,
-    target: "browser",
+  entrypoints: ["./src/index.ts"],
+  outdir: "./dist",
+  minify: true,
+  target: "node",
+  external: ["node:crypto"],
 });
